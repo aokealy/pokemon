90b24f6ccac963eb93f8b59ac0e9420235ad242a
aokealy <104302143+aokealy@users.noreply.github.com>
2022-12-05 17:31:46 +0000

    urls
    we connected the urls of home and room so we no longer get django template

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('room/', room)
+    path('', include('main.urls'))
+
 ]
