e5c7d71c704b566ffbc933adb95fbc7e8293abff
aokealy <104302143+aokealy@users.noreply.github.com>
2022-12-05 19:31:31 +0000

    models.py
    create a  user and room relationship one to many and many to one

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -17,7 +18,7 @@ class Room(models.Model):
 
 
 class Message(models.Model):
-    # user =
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
