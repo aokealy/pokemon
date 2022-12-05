64cca280562d591e41dad6c2004648e0fef80287
aokealy <104302143+aokealy@users.noreply.github.com>
2022-12-05 18:40:50 +0000

    update
    update the room template and them links

--- main/views.py
+++ main/views.py
@@ -14,5 +14,11 @@ def home(request):
     return render(request, 'main/home.html', context)
 
 
-def room(request):
-    return render(request, 'main/room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+
+    return render(request, 'main/room.html', context)
