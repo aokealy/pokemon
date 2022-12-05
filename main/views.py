b0838d1fcca22fd058fd58ea4f32a244ceabd534
aokealy <104302143+aokealy@users.noreply.github.com>
2022-12-05 19:18:12 +0000

    update views.py
    update the room.objects

--- main/views.py
+++ main/views.py
@@ -1,24 +1,22 @@
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Pokemon Cards'},
-    {'id': 2, 'name': 'Graded Cards'},
-    {'id': 3, 'name': 'Different Pokemon'},
-]
+# rooms = [
+#{'id': 1, 'name': 'Pokemon Cards'},
+#{'id': 2, 'name': 'Graded Cards'},
+#{'id': 3, 'name': 'Different Pokemon'},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'main/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
 
     return render(request, 'main/room.html', context)
