20fefab9a9afbe33091d2e5acd54edbc0710d672
aokealy <104302143+aokealy@users.noreply.github.com>
2022-12-05 18:18:09 +0000

    views.py
    create a var called rooms and add room names

--- main/views.py
+++ main/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Pokemon Cards'},
+    {'id': 2, 'name': 'Graded Cards'},
+    {'id': 3, 'name': 'Different Pokemon'},
+]
+
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 
 def room(request):
