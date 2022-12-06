8f9654d17bc73c9a01e49ccce54d22c6d4f1b54f
aokealy <104302143+aokealy@users.noreply.github.com>
2022-12-06 12:57:51 +0000

    views.py
    create an authenticate for user if not correct we add the error and when a user logins they are redirected to home

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -12,6 +13,8 @@ from .forms import RoomForm
 #{'id': 2, 'name': 'Graded Cards'},
 #{'id': 3, 'name': 'Different Pokemon'},
 # ]
+
+# login system
 
 
 def loginPage(request):
@@ -24,6 +27,14 @@ def loginPage(request):
             user = User.objects.get(username=username)
         except:
             messages.error(request, 'User does not exist!.')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Pasword does not exist.')
 
     context = {}
     return render(request, 'main/login_register.html', context)
