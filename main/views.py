ccb862f25c113335f3aa5c7ea4ec774cc7ed22be
aokealy <104302143+aokealy@users.noreply.github.com>
2022-12-12 10:16:28 +0000

    views.py
    index the topics from 0:5 so when we are in mobile it will display the first five

--- main/views.py
+++ main/views.py
@@ -76,7 +76,7 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
     )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -197,5 +197,6 @@ def updateUser(request):
 
 
 def topicsPage(request):
-    topics = Topic.objects.filter()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
     return render(request, 'main/topics.html', {'topics': topics})
