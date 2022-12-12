9ae10ea9efa72e13db8b6a29047f4e45a7cb9b89
aokealy <104302143+aokealy@users.noreply.github.com>
2022-12-12 11:18:09 +0000

    Update views.py
    add safe which means we can use more than python dictionaries inside of this response

--- main/api/views.py
+++ main/api/views.py
@@ -4,6 +4,6 @@ from django.http import JsonResponse
 def getRoutes(request):
     routes = [
         'GET /api/rooms',
-        'GET /api/rooms/:id',
+        'GET /api/rooms/:id'
     ]
-    return JsonResponse()
+    return JsonResponse(routes, safe=False)
