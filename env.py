ce865233be004026ea3a53999a0550667e54b38e
aokealy <104302143+aokealy@users.noreply.github.com>
2022-12-20 09:16:59 +0000

    Update env.py

--- env.py
+++ env.py
@@ -1,2 +1,2 @@
 import os
-os.environ["DATABASE_URL"] = "postgres://flgxdqqc:e3Xax081zO8MGGmEra0UMLnO9Q7hROsC@mel.db.elephantsql.com/flgxdqqc"
+os.environ["DATABASE_URL"] = "postgres://fyblddmooolviw:496650ff2f73728bc0dca1ec54d8221cd8d423b478f103e22c546d4fa9505ba8@ec2-18-214-134-226.compute-1.amazonaws.com:5432/dfqku863e4qao0"
